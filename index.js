32c177641ad2884cbe59d30e0b18d617790e3f90
danisdnk <danisdnk@gmail.com>
2021-10-09 03:34:09 -0300

    inicializo coneccion a dv y lectura del body

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const { dbConnection } = require('./database/config');
 dotenv.config();
 
 //crear servidor de Express
 const app = express();
+
+//base de datos
+dbConnection();
 //directorio publico
 app.use(express.static('public'));
+//lectura y parseo de body
+app.use(express.json());
 //rutas
 app.use('/api/auth/', require('./routes/auth'));
 
