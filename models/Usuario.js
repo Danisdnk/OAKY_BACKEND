4b3b328e7a037a4ff434c59fa64a29a855517363
danisdnk <danisdnk@gmail.com>
2021-10-09 03:35:58 -0300

    creo model de usuario prueba

---
+++ models/Usuario.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UsuarioSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+
+});
+module.exports =  mongoose.model('Usuario', UsuarioSchema);
