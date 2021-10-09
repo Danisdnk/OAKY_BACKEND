2b2849593aef7eaa84526230c9237f3e69558dc0
danisdnk <danisdnk@gmail.com>
2021-10-09 03:36:11 -0300

    modifico routing

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,19 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 
-router.get('/', (req, res) => {
 
-    res.json({
-        ok: true
-    });
-});
+router.post('/new',
+    [
+        check('name', 'El nombre es obligatorio').notEmpty(),
+        check('email', 'El nombre es obligatorio').isEmail(),
+        validarCampos
+    ],
+    crearUsuario);
+
+router.post('/', loginUsuario);
+
+router.get('/renew', revalidarToken);
 module.exports = router;
